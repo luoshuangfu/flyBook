57f28d69808781e119cd3b46bb33b13db205a777
liwenxuan-seven <69592215+liwenxuan-seven@users.noreply.github.com>
2026-04-06 09:28:16 +0800

    Delete shion-app-watcher-c0741b6 directory

--- shion-app-watcher-c0741b6/build.rs
+++
@@ -1,8 +0,0 @@
-const COMMANDS: &[&str] = &["get_program_list", "get_program_by_path", "suspend", "resume", "is_active"];
-
-fn main() {
-  tauri_plugin::Builder::new(COMMANDS)
-    .android_path("android")
-    .ios_path("ios")
-    .build();
-}
--- shion-app-watcher-c0741b6/src/shared.rs
+++
@@ -1,8 +0,0 @@
-use serde::{Deserialize, Serialize};
-
-#[derive(Deserialize, Serialize, Debug)]
-pub struct Program {
-    pub path: String,
-    pub name: String,
-    pub icon: Vec<u8>,
-}
